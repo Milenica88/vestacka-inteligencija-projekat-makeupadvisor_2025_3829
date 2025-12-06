6ddc9125ebd1c619347fe21f3329f39e15f6ff29
Milena Paripović <75785070+Milenica88@users.noreply.github.com>
2025-12-06 07:36:40 +0100

    dodatno

--- src/vestacka_inteligencija_projekat_makeupadvisor_2025_3829/core.clj
+++ src/vestacka_inteligencija_projekat_makeupadvisor_2025_3829/core.clj
@@ -9,3 +9,14 @@
     (= skin-type "mesovita") "Semi-matte foundation"
     (= skin-type "osetljiva") "Hypoallergenic foundation"
     :else "Universal lightweight foundation"))
+
+(defn shade
+  [podton svetlina]
+  (int (max 1 (min 10 (+ podton svetlina)))))
+
+(defn average
+  [populacija procenti]
+  (let [avg (/ (reduce + procenti) (count procenti))]
+    (int (* populacija avg))))
+
+
--- test/vestacka_inteligencija_projekat_makeupadvisor_2025_3829/core_test.clj
+++ test/vestacka_inteligencija_projekat_makeupadvisor_2025_3829/core_test.clj
@@ -7,3 +7,15 @@
     (is (= "Hydrating foundation" (recommend-foundation "suva")))
     (is (= "Oil-free matte foundation" (recommend-foundation "masna")))
     (is (= "Universal lightweight foundation" (recommend-foundation "nepoznato")))))
+
+(deftest shade-test
+  (testing
+    (is (= 1 (shade -2 3)))
+    (is (= 7 (shade 5 2)))))
+
+
+(deftest average-test
+  (testing
+    (is (= 5166 (average
+                  10000
+                  [0.57 0.38 0.60])))))
